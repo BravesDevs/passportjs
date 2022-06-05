47066251e85d223254707a4afa61840bd98a770b
Devang Parekh <devang.parekh@marutitech.com>
2022-06-05 17:46:35 +0530

    OAuth Setup

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const app = express();
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const { json } = require('body-parser');
+const passport = require('passport');
+
+//Passport Setup Config
+require('./passport')
+
+//Dot ENV Configuration
 require('dotenv').config()
 
 
 //Middlewares
+app.use(passport.initialize())
 app.use(bodyparser.urlencoded({ extended: true }))
 app.use(bodyparser.json())
 
@@ -19,5 +26,26 @@ app.get('/', (req, res, next) => {
         'message': 'Welcome to OAuth Demonstration.'
     })
 })
+
+app.get('/success', (req, res, nex) => {
+    res.status(200), json({
+        'ok': true,
+        'message': `Welcome ${req.user.email}`
+    })
+})
+
+app.get('/failed', (req, res, next) => {
+    res.status(500), json({
+        'ok': false,
+        'message': 'Connect Failed.'
+    })
+})
+
+app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }), (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/success');
+});
 
 app.listen(process.env.PORT, () => console.log(`Server started listening in ${process.env.PORT}`))
---
+++ passport.js
@@ -0,0 +1,14 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.OAUTH_CLIENT_ID,
+    clientSecret: process.env.OAUTH_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/google/callback"
+},
+    function (accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return done(err, profile);
+        });
+    }
+));
